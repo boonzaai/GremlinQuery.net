74eef13c3673b9c3225de5356cd3a6d725c131bc
Andre Boonzaaijer <andre.boonzaaijer@icris.nl>
2017-08-23 21:17:23 +0200

    Added XML doc output to nuget pckg and extended it.

--- Icris.GremlinQuery/Icris.GremlinQuery/g.cs
+++ Icris.GremlinQuery/Icris.GremlinQuery/g.cs
@@ -6,24 +6,54 @@ using System.Threading.Tasks;
 
 namespace Icris.GremlinQuery
 {
+    /// <summary>
+    /// Entry of the graph. Contains methods to query a graph's vertices, edges, or add items to both.
+    /// Reminder; if you want to use syntactical comparer support add the following using statement to your class:
+    /// using static Icris.GremlinQuery.Comparers;
+    /// </summary>
     public static class g
     {
+        /// <summary>
+        /// Get the graph's vertices. If you specify an id, you'll get the single vertex with this id as a result.
+        /// </summary>
+        /// <param name="id">String id.</param>
+        /// <returns></returns>
         public static IVertexResult V(string id = null)
         {
             return new Query().V(id);
         }
+        /// <summary>
+        /// Get the graph's vertex with the specified int id.
+        /// </summary>
+        /// <param name="id">int id.</param>
+        /// <returns></returns>
         public static IVertexResult V(int id)
         {
             return new Query().V(id);
         }
+        /// <summary>
+        /// Get the graph's edges
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public static IEdgeResult E(string id = null)
         {
             return new Query().E(id);
         }
+        /// <summary>
+        /// Add a vertex to the graph with a certain label. Extend the expression with a number of property() statements.
+        /// </summary>
+        /// <param name="label">Label of the newly added vertex. (i.e. "person")</param>
+        /// <returns></returns>
         public static IVertexResult addV(string label = null)
         {
             return new Query().addV(label);
         }
+        /// <summary>
+        /// Add an edge to the graph with a certain label. Extend the expression with a number of property() statements.
+        /// </summary>
+        /// <param name="label">Label of the newly added ege. (i.e. "owns")</param>
+        /// <returns></returns>
         public static IEdgeResult addE(string label = null)
         {
             return new Query().addE(label);
