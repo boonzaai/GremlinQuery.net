c011b3d84930d2537cfeeb03fc8040aafeca1070
Jacob Craft <jacob.craft@careotter.onmicrosoft.com>
2017-10-06 16:27:53 -0500

    added missing summary comments

--- Icris.GremlinQuery/Icris.GremlinQuery/IParameterResult.cs
+++ Icris.GremlinQuery/Icris.GremlinQuery/IParameterResult.cs
@@ -23,9 +23,21 @@ namespace Icris.GremlinQuery
         /// <param name="id"></param>
         /// <returns></returns>
         IParameterResult hasId(int id);
+        /// <summary>
+        /// Remove the traverser if its element does not have the label
+        /// </summary>
         IParameterResult hasLabel(string label);
+        /// <summary>
+        /// Move to the vertex that was not the vertex that was moved from
+        /// </summary>
         IParameterResult otherV();
+        /// <summary>
+        /// Performs an out going traversal step
+        /// </summary>
         IParameterResult @out();
+        /// <summary>
+        /// Returns the history of the traversed path without repeating traversals
+        /// </summary>
         ParameterQuery simplePath();
     }
 }
