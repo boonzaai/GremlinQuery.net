befeca636e3039239b203355fdba6184e7c61d9e
Jacob Craft <jacob.craft@careotter.onmicrosoft.com>
2017-10-06 16:20:58 -0500

    removed proof of concept test and added parameterQuery tests.

---
+++ Icris.GremlinQuery/Icris.GremlinQuery.Tests/ParameterQueryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Icris.GremlinQuery.Tests
+{
+    /// <summary>
+    /// Summary description for ParameterQueryTests
+    /// </summary>
+    [TestClass]
+    public class ParameterQueryTests
+    {
+        [TestMethod]
+        public void ParameterQuery_QueryHasNoPeriods()
+        {
+            Assert.AreEqual(
+                "out()",
+                Parameter.@out().ToString()
+                );
+        }
+        
+        [TestMethod]
+        public void ParameterQuery_QueryChainsHavePeriods()
+        {
+            Assert.AreEqual(
+                "out().simplePath()",
+                Parameter.@out().simplePath().ToString()
+                );
+        }
+
+        [TestMethod]
+        public void ParameterQuery_QueryBuiltProperlyInArgument()
+        {
+            Assert.AreEqual(
+                "g.V('123').repeat(out().simplePath()).until(hasId('5')).path().limit(1)",
+                g.V("123").repeat(Parameter.@out().simplePath()).until(Parameter.hasId("5")).path().limit(1).ToString()
+                );
+        }
+    }
+}
---
+++ icris.gremlinquery/icris.gremlinquery.tests/querytestset.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Icris.GremlinQuery.Comparers;
+
+namespace Icris.GremlinQuery.Tests
+{
+    [TestClass]
+    public class QueryTestSet
+    {
+        [TestMethod]
+        public void Queries_GremlinTinkerPopWorkout_FirstFiveMinutes()
+        {
+            //Node addition. Pre-condition to running all statements below (which we won't do here but nonetheless, it doesn't seem right not to do test this in the first place).
+            var expected = "g.addV('person').property('name','marko').property('age',29).property('id',1)";
+            var actual = g.addV("person").property("name", "marko").property("age", 29).property("id", 1).ToString();
+            Assert.AreEqual(expected, actual);
+
+            //All vertices
+            Assert.AreEqual(
+                "g.V()",
+                g.V().ToString()
+                );
+            //single vertex
+            Assert.AreEqual(
+                "g.V(1)",
+                g.V(1).ToString()
+                );
+            //get vertex 1's name property.
+            Assert.AreEqual(
+                "g.V(1).values('name')",
+                g.V(1).values("name").ToString()
+                );
+            //get edges with label 'knows' for vertex 1
+            Assert.AreEqual(
+                "g.V(1).outE('knows')",
+                g.V(1).outE("knows").ToString()
+                );
+            //Names of the people 1 knows
+            Assert.AreEqual(
+                "g.V(1).outE('knows').inV().values('name')",
+                g.V(1).outE("knows").inV().values("name").ToString()
+                );
+
+            Assert.AreEqual(
+                "g.V(1).out('knows').values('name')",
+                g.V(1).@out("knows").values("name").ToString()
+                );
+
+            Assert.AreEqual(
+                "g.V(1).out('knows').has('age',gt(30)).values('name')",
+                g.V(1).@out("knows").has("age", gt(30)).values("name").ToString()
+                );
+
+        }
+
+        [TestMethod]
+        public void Queries_GremlinTinkerPopWorkout_NextFifteenMinutes()
+        {
+            //Find vertices with name = marko
+            Assert.AreEqual(
+                "g.V().has('name','marko')",
+                g.V().has("name", "marko").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name','marko').outE('created')",
+                g.V().has("name", "marko").outE("created").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name','marko').outE('created').inV()",
+                g.V().has("name","marko").outE("created").inV().ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name','marko').out('created')",
+                g.V().has("name", "marko").@out("created").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name','marko').out('created').values('name')",
+                g.V().has("name","marko").@out("created").values("name").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name',within('vadas','marko')).values('age').mean()",
+                g.V().has("name", within("vadas","marko")).values("age").mean().ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name','marko').out('created')",
+                g.V().has("name", "marko").@out("created").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().has('name','marko').out('created').in('created').values('name')",
+                g.V().has("name", "marko").@out("created").@in("created").values("name").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().as('a').out().as('b').out().as('c').select('a','b','c')",
+                g.V().@as("a").@out().@as("b").@out().@as("c").select("a","b","c").ToString()
+                );
+            Assert.AreEqual(
+                "g.V().group().by(label).by('name')",
+                g.V().group().by(property.label).by("name").ToString()
+                );
+
+
+        }
+    }
+}
